5099cb28befa9f24389a285f487417649bd28ae2
Albert Pratomo <albertpratomo21@gmail.com>
2024-06-02 11:13:09 +0200

    feat: color the board

--- components/Board.vue
+++ components/Board.vue
@@ -1,18 +1,33 @@
 <script setup lang="ts">
 const rows = [8, 7, 6, 5, 4, 3, 2, 1]
 const cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+
+const style = {
+    '--dark': '#739552',
+    '--light': '#eaebcf',
+}
+
+function getColor(x: number, y: number) {
+    return (x + y) % 2 === 0
+        ? 'bg-[--light] text-[--dark]'
+        : 'bg-[--dark] text-[--light]'
+}
 </script>
 
 <template>
-    <div class="flex flex-col gap-1">
+    <div
+        class="flex flex-col"
+        :style="style"
+    >
         <div
             v-for="(row, y) in rows" :key="row"
-            class="flex gap-1"
+            class="flex"
         >
             <div
                 v-for="(col, x) in cols"
                 :key="col"
-                class="relative size-20 bg-slate-200"
+                class="relative size-20 font-bold"
+                :class="getColor(x, y)"
             >
                 <div
                     v-if="col === 'a'"

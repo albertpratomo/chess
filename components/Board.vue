6bbaf35ff43582383fad7dad894e560980c12ff5
Albert Pratomo <albertpratomo21@gmail.com>
2024-06-04 01:06:21 +0200

    feat: show last clicked square

--- components/Board.vue
+++ components/Board.vue
@@ -5,6 +5,7 @@ const cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 const style = {
     '--dark': '#739552',
     '--light': '#eaebcf',
+    '--highlight': 'rgba(255, 255, 51, 0.5)',
 }
 
 function getColor(x: number, y: number) {
@@ -12,13 +13,16 @@ function getColor(x: number, y: number) {
         ? 'bg-[--light] text-[--dark]'
         : 'bg-[--dark] text-[--light]'
 }
+
+const lastClickedSquare = ref('')
+
+function onClick(square: string) {
+    lastClickedSquare.value = square
+}
 </script>
 
 <template>
-    <div
-        class="flex flex-col"
-        :style="style"
-    >
+    <div :style="style">
         <div
             v-for="(row, y) in rows" :key="row"
             class="flex"
@@ -26,22 +30,28 @@ function getColor(x: number, y: number) {
             <div
                 v-for="(col, x) in cols"
                 :key="col"
-                class="relative size-20 font-bold"
+                class="relative size-20 cursor-pointer font-bold"
                 :class="getColor(x, y)"
+                @click="onClick(`${col}${row}`)"
             >
-                <div
+                <span
                     v-if="col === 'a'"
                     class="absolute left-2 top-1"
                 >
                     {{ row }}
-                </div>
+                </span>
 
-                <div
+                <span
                     v-if="row === 1"
                     class="absolute bottom-1 right-2"
                 >
                     {{ col }}
-                </div>
+                </span>
+
+                <div
+                    v-if="lastClickedSquare === `${col}${row}`"
+                    class="size-full bg-[--highlight]"
+                />
             </div>
         </div>
     </div>
